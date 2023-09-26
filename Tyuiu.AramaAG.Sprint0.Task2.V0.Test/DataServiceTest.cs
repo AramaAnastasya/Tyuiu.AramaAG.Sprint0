ae5baddad8b13ffc4bdba15349be1a6e38c68bce
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-09-26 18:12:46 +0500

    Task2

---
+++ Tyuiu.AramaAG.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AramaAG.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.AramaAG.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessage()
+        {
+            var name = "Анастасия";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Анастасия", res);
+        }
+    }
+}
